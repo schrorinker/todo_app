ceb9afbcb0b5b55bb217f8fcac0e9160fe98f177
EC2 Default User <ec2-user@ip-172-31-42-179.ap-northeast-1.compute.internal>
2021-02-24 00:06:02 +0000

    add controller & model & view

---
+++ database/migrations/2021_02_23_221714_create_todos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('content');
+            $table->integer('goal_id');
+            $table->integer('position');
+            $table->boolean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
